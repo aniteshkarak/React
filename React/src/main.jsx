4481cdd93c29e5cf6a724d0b7ffbb64557b1494d
Anitesh Karak <209986326+aniteshkarak@users.noreply.github.com>
2026-07-21 17:27:01 +0530

    anothor custom function

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+const element = (
+  <a href="https://bharatpg.site" target='_blank'>Visit Best Pg in KGP</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://bharatpg.site', target: '_blank'},
+  'click Me to visit'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+  reactElement
+  
 )
