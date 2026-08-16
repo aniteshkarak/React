0729d1291449e2f0425187b714a778cbfcad9c81
Anitesh Karak <aniteshkarak@gmail.com>
2026-08-16 22:44:51 +0530

    Add initial project setup with React, Vite, and Tailwind CSS
    - Created main entry point with React and StrictMode
    - Added Vite configuration for React and Tailwind CSS
    - Included global CSS variables for light and dark themes
    - Added SVG assets for React and Vite logos
    - Established basic styling for typography and layout

---
+++ TailwindProps/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="bg-green-400 rounded-3xl p-4">Tailwind Text</h1>
+    </>
+  )
+}
+
+export default App
