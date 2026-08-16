6ef891af2963f3a11f447a7994a890d725095da5
Anitesh Karak <aniteshkarak@gmail.com>
2026-08-16 23:10:38 +0530

    feat: add Card component to display product information and images

--- TailwindProps/src/App.jsx
+++ TailwindProps/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './Components/card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,9 @@ function App() {
   return (
     <>
       <h1 className="bg-green-400 rounded-3xl p-4">Tailwind Text</h1>
+      <Card/>
+      <Card/>
+      
     </>
   )
 }
