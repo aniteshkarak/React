1af4735d26047315cd60db4a2a73492f67634906
Anitesh Karak <aniteshkarak@gmail.com>
2026-08-17 20:25:40 +0530

    feat: enhance Card component to accept props for username and button text using Tailwind

--- TailwindProps/src/App.jsx
+++ TailwindProps/src/App.jsx
@@ -7,12 +7,16 @@ import Card from './Components/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "anitesh",
+    age: 21
+  }
 
   return (
     <>
       <h1 className="bg-green-400 rounded-3xl p-4">Tailwind Text</h1>
-      <Card/>
-      <Card/>
+      <Card username="Time4Code" btnText="Click Me"/>
+      <Card username="Jinda Hu"/>
       
     </>
   )
--- TailwindProps/src/Components/card.jsx
+++ TailwindProps/src/Components/card.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-function Card(){
+function Card({username,btnText="Visit Again"}){
+  // console.log("props",props);
+  // console.log(props.username);
+  console.log(username);
     return(
         <div className="relative h-[400px] w-[300px] rounded-b-md m-5">
         <img 
@@ -10,10 +13,10 @@ function Card(){
         />
         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
         <div className="absolute bottom-4 left-4 text-left">
-          <h1 className="text-lg font-semibold text-white">AirMax Pro</h1>
+          <h1 className="text-lg font-semibold text-white">{username}</h1>
           <p className="mt-2 text-sm text-gray-300">Experience the future of running with our latest innovation.</p>
           <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-            Learn More
+            {btnText}
           </button>
         </div>
 
