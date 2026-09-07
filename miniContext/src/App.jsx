1ff4d1480b8b906a3a17d8db63f08265bf5ae885
Anitesh Karak <aniteshkarak@gmail.com>
2026-09-07 19:58:47 +0530

    feat: implement UserContext and UserContextProvider for user state management

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React with Anitesh</h1>
-    </>
+    </UserContextProvider>
   )
 }
+
+
 
 export default App
---
+++ miniContext/src/Context/UserContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
+
+// Provider is a component that will wrap the components that need access to the context. 
+// It will provide the context value to the components that are wrapped inside it.
+
+<UserContext>
+    <login/>
+    <Card>
+        <Dashboard/>
+    </Card>
+</UserContext>
