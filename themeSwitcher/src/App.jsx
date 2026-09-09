23f62eda47257eef755306be72508b0d325bc45f
Anitesh Karak <aniteshkarak@gmail.com>
2026-09-09 20:49:46 +0530

    feat: implement theme switching functionality with context and components

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { ThemeProvider } from './Context/theme'
+import Card from './Component/Card'
+import ThemeBtn from './Component/ThemeBtn'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // acutal Change of theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+    // document.documentElement.classList.remove("light", "dark")
+    // document.documentElement.classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
 
-    </>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
