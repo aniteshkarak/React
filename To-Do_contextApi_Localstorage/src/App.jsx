e31d00ed2510cb431a04f98fe16c940577f8a8ab
Anitesh Karak <aniteshkarak@gmail.com>
2026-09-16 22:03:13 +0530

    feat: add localStorage integration for todos and enhance toggle functionality

--- To-Do_contextApi_Localstorage/src/App.jsx
+++ To-Do_contextApi_Localstorage/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { TodoProvider } from './Contexts'
 
@@ -17,7 +17,23 @@ function App() {
 
   const toggleCompleted = (id) => {
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo)))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}>
